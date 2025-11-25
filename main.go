93bbfdd75be671ef8542c69634f34ccfd5cc2b47
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2025-11-25 14:06:30 +0800

    add health endpoint
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "net/http"
 
+func handleHealth(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handleHealth)
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
