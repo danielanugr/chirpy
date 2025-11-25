fb90e40ed72738ab68a844b9505a613255fc0075
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2025-11-25 14:23:34 +0800

    add metric hits and reset metric endpoint
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func handleHealth(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -8,11 +16,37 @@ func handleHealth(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content_Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	mux.HandleFunc("/healthz", handleHealth)
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
